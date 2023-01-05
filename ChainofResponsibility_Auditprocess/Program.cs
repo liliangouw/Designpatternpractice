c447bb26eec2d0f724129492e4e2106228470245
liliangouw <1803443590@qq.com>
2023-01-05 17:33:44 +0800

    审核流程可配置开篇

---
+++ ChainofResponsibility_Auditprocess/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ChainofResponsibility_Auditprocess
+{
+    /// <summary>
+    /// 责任链模式实现审核流程可配置
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
